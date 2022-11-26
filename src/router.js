b24d97cb1ca3bf6ed95bbd0d3d70cf7494c2c21b
nickbevers <nick.bevers9@gmail.com>
2022-11-26 20:59:32 +0100

    feat(router): add login page to the router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Configurator from './views/Configurator.vue'
 import Dashboard from './views/Dashboard.vue'
+import Login from './views/Login.vue'
 
 export default createRouter({
     history: createWebHistory(),
@@ -20,6 +21,15 @@ export default createRouter({
             path: '/dashboard',
             name: 'Dashboard',
             component: Dashboard,
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login,
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            redirect: '/',
         },
     ]
 })
