2fa95b932c5938f225f25818e785893adb5509d1
nickbevers <nick.bevers9@gmail.com>
2022-11-26 22:10:11 +0100

    feat(login): add login form component with 2way data binding and simple submit logic

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import { ref } from 'vue'
+    import { login } from '../assets/functions/login';
+
+</script>
+
+<template>
+    <div class="form__container">
+        <form @submit.prevent="handleSubmit">
+            <div class="form__item">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" v-model="email"/>
+            </div>
+            <div class="form__item">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Password" v-model="password"/>
+            </div>
+            <div class="form__item">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoginForm',
+        data() {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+        methods: {
+            handleSubmit() {
+                console.log("submitted form");
+            }
+        }
+    }
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,13 @@
 <script setup>
     import Navigation from '../components/Navigation.vue';
-    import { login } from '../assets/functions/login';
-
-    const loginFunction = () => {
-        login("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5iZSIsImlhdCI6MTY2OTQ5MTY1MX0.wYbQ6QQxJq4a8uesyije7PS8PfywSTZO2PwDPAQhP0M", "test@test.be", "test1234");
-    }
+    import LoginForm from '../components/LoginForm.vue';
+    // import { login } from '../assets/functions/login';
 
 </script>
 
 <template>
     <div class="">
         <Navigation />
-
-        <button @click="loginFunction">Click me to login!</button>
+        <LoginForm />
     </div>
 </template>
