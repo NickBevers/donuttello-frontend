41c2ab0b96c1b1cc671b3f18a1c7267ccf43c295
nickbevers <nick.bevers9@gmail.com>
2022-11-26 20:14:30 +0100

    feat(Home cta button): add cta button to get customers to go to the configurator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,14 @@
     <div class="main__container">
         <Navigation />
         <section class="text__container">
-            <h1>Verse Vlaamse Donuts in de Stijl <br> van Jouw Bedrijf als Onvergetelijke <br> Attentie of Heerlijk Dessert</h1>
-            <p>Creëer je eigen donut <a href="#/configurator">hier</a></p>
+            <h1 class="text__title">Verse Vlaamse Donuts in de Stijl van Jouw Bedrijf als Onvergetelijke Attentie of Heerlijk Dessert</h1>
+            <p class="text__detail">Creëer je eigen donut om een idee te krijgen van wat je kan verwachten bij ons. <br>
+                Of bestel hem meteen om hem te laten bezorgen.
+            </p>
+
+            <div class="button__container">
+                <a href="/configurator" class="button__text">Creëer je eigen donut</a>
+            </div>
         </section>
     </div>
 </template>
-
-<style scoped>
-
-</style>
