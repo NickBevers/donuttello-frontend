642ac431e038446d1b83c15f0dd4f5836c7d3eb2
nickbevers <nick.bevers9@gmail.com>
2022-11-26 21:00:20 +0100

    feat(login page): add login page so that admins can log themselves in

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import Navigation from '../components/Navigation.vue';
+    import { login } from '../assets/functions/login';
+
+    const loginFunction = () => {
+        login("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5iZSIsImlhdCI6MTY2OTQ5MTY1MX0.wYbQ6QQxJq4a8uesyije7PS8PfywSTZO2PwDPAQhP0M", "test@test.be", "test1234");
+    }
+
+</script>
+
+<template>
+    <div class="">
+        <Navigation />
+
+        <button @click="loginFunction">Click me to login!</button>
+    </div>
+</template>
