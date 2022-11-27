90de62afa30b5a9fc4ce94c9aa4d5f9b2f5b389c
nickbevers <nick.bevers9@gmail.com>
2022-11-27 18:50:34 +0100

    feat(loginPassword): add password field with visibility toggle and forgot password link

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,22 +1,36 @@
 <script setup>
-    import { ref } from 'vue'
-    import { login } from '../assets/functions/login';
-
+    import { ref } from 'vue';
+    
+    const showPassword = ref(true);
+    const togglePassword = () => {
+        showPassword.value = !showPassword.value;
+    }
 </script>
 
 <template>
     <div class="form__container">
         <form @submit.prevent="handleSubmit">
             <div class="form__item">
-                <label for="email">Email</label>
+                <label for="email">E-mailadres</label>
                 <input type="email" id="email" name="email" placeholder="Email" v-model="email"/>
             </div>
             <div class="form__item">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" v-model="password"/>
+                <label for="password">Paswoord</label>
+                <div class="form__item--password">
+                    <input v-if="showPassword" type="text" id="password" name="password" placeholder="Password" v-model="password"/>
+                    <input v-else type="password" id="password" name="password" placeholder="Password" v-model="password"/>
+                    
+                    <font-awesome-icon @click="togglePassword" v-if="showPassword" class="icon--eye" icon="fa-solid fa-eye-slash" />
+                    <font-awesome-icon @click="togglePassword" v-else class="icon--eye" icon="fa-solid fa-eye" />
+                </div>
             </div>
-            <div class="form__item">
-                <button type="submit">Login</button>
+
+            <div class="form__item form__item--forgot">
+                <a href="/forgot-password">Paswoord vergeten?</a>
+            </div>
+
+            <div class="form__item form__item--submit">
+                <button type="submit">Inloggen</button>
             </div>
         </form>
     </div>
