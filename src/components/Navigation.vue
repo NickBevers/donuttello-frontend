3d5bb96cc7a39a880d79d4a7ac3522d46b37fd99
nickbevers <nick.bevers9@gmail.com>
2022-11-27 18:45:29 +0100

    fix(fontAwesome): change font awesome implementation to be for the whole project

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,3 @@
-<script setup>
-    import { ref, computed } from 'vue'
-    import { library } from '@fortawesome/fontawesome-svg-core'
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-    import { faUser } from '@fortawesome/free-solid-svg-icons';
-    library.add(faUser);
-
-</script>
-
 <template>
     <nav>
         <div class="navigation__container">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import "./assets/sass/main.scss";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUser, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUser, faEye, faEyeSlash);
+
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .mount('#app')
