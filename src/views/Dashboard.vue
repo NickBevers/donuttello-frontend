1a53063102ec5a2ec66756bcafc59ad96785bcf9
nickbevers <nick.bevers9@gmail.com>
2022-11-27 21:30:49 +0100

    feat(dashboard): add fetch to get all the donuts and show them on screen (pure json)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,41 @@
 <script setup>
-    import { ref } from 'vue'
+    import { onMounted, ref } from 'vue'
     import Navigation from '../components/Navigation.vue';
 
     const jwtToken = ref(localStorage.getItem('jwtToken'));
+    const donuts = ref([]);
+
     if(!jwtToken.value) { window.location.href = '/login'; }
+
+    const getDonuts = () => {
+        fetch("https://donuttello-backend.onrender.com/api/v1/donuts", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken.value}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "success") {
+                donuts.value = data.data;
+            } else {
+                console.log(data);
+            }
+        });
+    }
+
+    onMounted(() => {
+        getDonuts();
+    })
 </script>
 
 <template>
     <div class="home">
         <Navigation />
         <p class="homeText">Dashboard</p>
+        <pre v-if="donuts.length > 0">
+            {{ donuts }}
+        </pre>
     </div>
 </template>
-
-<style scoped>
-
-</style>
