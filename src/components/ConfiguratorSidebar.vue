10f2311f02559960b3c7f5a96582c053fdbc7369
nickbevers <nick.bevers9@gmail.com>
2022-11-30 20:16:26 +0100

    feat(dashboard): add filter dropdown and corresponding donuts with simple listing; feat(configurator): add a colorpicker to the configurator with a color variabnle to pass it through to the database later$

--- src/components/ConfiguratorSidebar.vue
+++ src/components/ConfiguratorSidebar.vue
@@ -1,10 +1,22 @@
 <script setup>
-import SidebarItem from './ConfgiruatorSidebarItems.vue';
+    import { ref } from 'vue'
+    import SidebarItem from './ConfgiruatorSidebarItems.vue';
+    import { ColorPicker } from 'vue-accessible-color-picker';
+
+    const color = ref('#000000');
+
+    function updateColor (e) {
+        color.value = e.cssColor;
+    }
 </script>
 
 <template>
-  <p>hey this works</p>
-  <SidebarItem />
+    <p>hey this works</p>
+    <SidebarItem />
+    <div class="colorpicker__container">
+        <ColorPicker :color="color" :visible-formats="['hex']" @color-change="updateColor"/>
+        {{ color }}
+    </div>
 </template>
 
 <style scoped>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,11 +4,32 @@
 
     const jwtToken = ref(localStorage.getItem('jwtToken'));
     const donuts = ref([]);
+    const filter = ref('order');
+
+    /*
+
+    function sorting() {
+  switch (filter.value) {
+    case "votes-high-to-low":
+      donuts.donuts.sort((a, b) => (b.votes > a.votes ? 1 : -1));
+      break;
+    case "votes-low-to-high":
+      donuts.donuts.sort((a, b) => (b.votes < a.votes ? 1 : -1));
+      break;
+    case "date-new-to-old":
+      donuts.donuts.sort((a, b) => new Date(b.date) > new Date(a.date) ? 1 : -1 );
+      break;
+    case "date-old-to-new":
+      donuts.donuts.sort((a, b) => new Date(b.date) < new Date(a.date) ? 1 : -1 );
+      break;
+  }
+}
+
+    */
 
     if(!jwtToken.value) { window.location.href = '/login'; }
-
     const getDonuts = () => {
-        fetch("https://donuttello-backend.onrender.com/api/v1/donuts", {
+        fetch(`https://donuttello-backend.onrender.com/api/v1/donuts?filter=${filter.value}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -33,9 +54,32 @@
 <template>
     <div class="home">
         <Navigation />
-        <p class="homeText">Dashboard</p>
-        <pre v-if="donuts.length > 0">
-            {{ donuts }}
-        </pre>
+        <div class="dashboard__container">
+            <div class="dashboard__container__header">
+                <h1>Dashboard</h1>
+                <div class="dashboard__container__header__filter">
+                    <label for="filter">Filter</label>
+                    <select name="filter" id="filter" v-model="filter" v-on:change="getDonuts">
+                        <option value="all">All</option>
+                        <option value="order" selected>Ordered</option>
+                        <option value="notOrder">Not Ordered</option>
+                    </select>
+                    {{ filter }}
+                </div>
+            </div>
+            <div class="dashboard__container__donuts">
+                <div class="dashboard__container__donuts__donut" v-for="donut in donuts" :key="donut.id">
+                    <div class="dashboard__container__donuts__donut__image">
+                        <img :src="donut.image" alt="donut">
+                    </div>
+                    <div class="dashboard__container__donuts__donut__info">
+                        <h2>{{ donut.name }}</h2>
+                        <p>{{ donut.description }}</p>
+                        <p>{{ donut.price }}</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </div>
 </template>
