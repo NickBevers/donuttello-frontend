17310b0feb15f9fa9da23cb60703182a9cbf21bc
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-02 17:42:55 +0100

    Refactor: change title, remove unnecessary element in component

---
+++ src/components/SideBar/GlaceFlavour.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+  <div class="confSidebar__item">
+    <header class="confSidebar__item__header">
+      <h3 class="confSidebar__item__header__heading">Glace flavour</h3>
+
+    </header>
+    <section class="confSidebar__item__section">
+      <ul class="confSidebar__item__section--listing">
+        <li class="listing__item">Option 1</li>
+        <li class="listing__item">Option 2</li>
+        <li class="listing__item">Option 3</li>
+        <li class="listing__item">Option 4</li>
+        <li class="listing__item">Option 5</li>
+        <li class="listing__item">Option x</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
