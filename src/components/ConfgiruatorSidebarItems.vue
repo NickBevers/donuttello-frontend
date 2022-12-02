0bb7f63d8a30dc989c41472bd59bb929f624e628
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-02 17:21:56 +0100

    Refactor: split ConfiguratorSidebarItems.vue into smaller components

--- src/components/ConfgiruatorSidebarItems.vue
+++ src/components/ConfgiruatorSidebarItems.vue
@@ -13,31 +13,6 @@ const test = () => {
 
 <template>
   <div class="confSidebar__item">
-  <header class="confSidebar__item__header">
-    <h3 class="confSidebar__item__header__heading">This is the header of the SidebarListItems</h3>
-    <input type="checkbox" id="checkbox" v-model="checked" @click="test"/>
-
-  </header>
-  <section class="confSidebar__item__section">
-    <ul class="confSidebar__item__section--listing">
-      <li class="listing__item">Option 1</li>
-      <li class="listing__item">Option 2</li>
-      <li class="listing__item">Option 3</li>
-      <li class="listing__item">Option 4</li>
-      <li class="listing__item">Option 5</li>
-      <li class="listing__item">Option x</li>
-    </ul>
-  </section>
-  <section>
-    This is the color picker section
-  </section>
-  <section class="confSidebar__item__section">
-    <label for="fileUpload" class="input__file__label">
-      Upload your logo here <br>
-      We accept png's, jpg's, jpeg's and webp's
-    </label>
-    <input class="input__file" type="file" id="fileUpload" name="fileUpload">
-  </section>
   <form action="">
     <textarea name="" id="" cols="30" rows="10">
       Here is a text field
---
+++ src/components/SideBar/UploadLogo.vue
@@ -0,0 +1,26 @@
+<script setup>
+import{ref} from "vue";
+const checked = ref(false);
+const test = () => {
+  console.log(checked.value);
+}
+</script>
+<template>
+  <div class="confSidebar__item">
+    <header class="confSidebar__item__header">
+      <h3 class="confSidebar__item__header__heading">This is the header of the SidebarListItems</h3>
+      <input type="checkbox" id="checkbox" v-model="checked" @click="test"/>
+    </header>
+    <section class="confSidebar__item__section">
+      <label for="fileUpload" class="input__file__label">
+        Upload your logo here <br>
+        We accept png's, jpg's, jpeg's and webp's
+      </label>
+      <input class="input__file" type="file" id="fileUpload" name="fileUpload">
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
