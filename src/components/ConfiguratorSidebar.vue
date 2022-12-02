d80ec60802a65162f61e9855efcc9c6a97bcc26c
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-02 17:48:35 +0100

    Refactor: rename component without typo's

--- src/components/ConfiguratorSidebar.vue
+++ src/components/ConfiguratorSidebar.vue
@@ -1,6 +1,6 @@
 <script setup>
     import { ref } from 'vue'
-    import SidebarItem from './ConfgiruatorSidebarItems.vue';
+    import SidebarItem from './ConfiguratorSidebarItems.vue';
 </script>
 
 <template>
