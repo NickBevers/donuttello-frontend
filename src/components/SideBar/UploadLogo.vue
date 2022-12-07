e259feec7cfe26f9aabc00509022b117afc5ab66
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-07 11:26:34 +0100

    Feat: show options when toggled to true

--- src/components/SideBar/UploadLogo.vue
+++ src/components/SideBar/UploadLogo.vue
@@ -1,18 +1,14 @@
 <script setup>
 import{ref} from "vue";
 const checked = ref(false);
-const test = () => {
-    checked.value = !checked.value;
-    console.log(checked.value);
-}
 </script>
 <template>
   <div class="confSidebar__item">
     <header class="confSidebar__item__header">
       <h3 class="confSidebar__item__header__heading">(Bedrijfs)logo</h3>
-      <input type="checkbox" id="checkbox" v-model="checked" @click="test"/>
+      <input type="checkbox" id="checkbox" v-model="checked" @click="toggle = !toggle"/>
     </header>
-    <section class="confSidebar__item__section">
+    <section class="confSidebar__item__section" v-if="toggle">
       <label for="fileUpload" class="input__file__label">
         Upload hier jouw logo <br>
         We accepteren png's, jpg's, jpeg's and webp's
