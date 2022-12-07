6f37d6cf25d92434ac8b15244dad15e634c041e0
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-07 11:03:27 +0100

    Feat: show options when toggled to true

--- src/components/SideBar/FillingList.vue
+++ src/components/SideBar/FillingList.vue
@@ -1,19 +1,14 @@
 <script setup>
 import{ref} from "vue";
-
-const checked = ref(false);
-const test = () => {
-  console.log(checked.value);
-}
 </script>
 <template>
   <div class="confSidebar__item">
     <header class="confSidebar__item__header">
       <h3 class="confSidebar__item__header__heading">Vulling</h3>
-      <input type="checkbox" id="checkbox" v-model="checked" @click="test"/>
+      <input type="checkbox" id="checkbox" v-model="checked" @click="toggle = !toggle"/>
 
     </header>
-    <section class="confSidebar__item__section">
+    <section v-if="!toggle" class="confSidebar__item__section">
       <ul class="confSidebar__item__section--listing">
         <li class="listing__item">Optie 1</li>
         <li class="listing__item">Optie 2</li>
