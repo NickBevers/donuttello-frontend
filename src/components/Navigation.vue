249d6801c9a96ebeb1dd42bfd568091b00eee055
nickbevers <nick.bevers9@gmail.com>
2022-12-10 10:52:39 +0100

    feat(adminCheck): add check to the navigation to see if a user is an Admin and show dashboard and resetpassword nav__items if so

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,27 @@
+<script setup>
+    import { ref } from 'vue'
+    const isAdmin = ref(false);
+    const jwtToken = ref(localStorage.getItem('jwtToken'));
+    
+    if(jwtToken.value){
+        fetch(`http://localhost:3000/api/v1/users/auth`, {
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken.value}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "success") {
+                isAdmin.value = true;
+                // console.log(data);
+            }
+        });
+    }
+</script>
+
 <template>
     <nav>
         <div class="navigation__container">
@@ -6,8 +30,9 @@
             </div>
             <div class="navigation__item__container">
                 <router-link class="navigation__item" exact to="/"><a>Home</a></router-link>
-                <router-link class="navigation__item" exact to="/dashboard"><a>Dashboard</a></router-link>
+                <router-link v-if="isAdmin" class="navigation__item" exact to="/dashboard"><a>Dashboard</a></router-link>
                 <router-link class="navigation__item" exact to="/configurator"><a>Configurator</a></router-link>
+                <router-link v-if="isAdmin" class="navigation__item" exact to="/forgot-password"><a>Wijzig Paswoord</a></router-link>
                 <router-link class="navigation__item navigation__item--icon" to="/login"><font-awesome-icon icon="fa-solid fa-user" /></router-link>
             </div>
         </div>
