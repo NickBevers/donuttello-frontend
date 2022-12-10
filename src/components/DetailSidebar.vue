6739e5c465ac5a20fe169d939f90fa5809372081
nickbevers <nick.bevers9@gmail.com>
2022-12-10 15:02:48 +0100

    feat(detail): add donutCanvas and sidebar to the detail vies

---
+++ src/components/DetailSidebar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="detailSidebar">
+    </div>
+</template>
+
+<style scoped>
+    .detailSidebar{
+        width: 30%;
+        height: calc(100vh - 5.7em);
+        overflow-y: auto;
+        scrollbar-width: thin;
+        scrollbar-color: var(--pink--main) var(--white);
+    }
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,7 +2,7 @@
 
     import { reactive, computed } from 'vue'
     import Navigation from '../components/Navigation.vue';
-    // import DonutCanvas from '../components/DonutCanvas.vue';
+    import DonutCanvas from '../components/DonutCanvas.vue';
     const donutId = window.location.href.split('/').pop();
 
     let donut = reactive({ data: {} });
@@ -33,12 +33,20 @@
     <div class="donut__detail">
         <Navigation />
 
-        <div class="donut__detail__container">
-            <!-- <DonutCanvas :donutData=donut.data /> -->
+        <div class="donut__detail__container configurator__canvas">
+            <DonutCanvas :donutData=donut.data />
         </div>
     </div>
 </template>
 
 <style scoped>
+.configurator__canvas {
+    width: 100%;
+    height: calc(100vh - 5.7em);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #cae3e8;
+}
 
 </style>
