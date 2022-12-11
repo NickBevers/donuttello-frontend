45bb4f02db40b433aaa0c9f3afb2ba29ae1eac8c
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-12-11 08:39:17 +0100

    Feat: add extra explaination to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // bestanden worden herkend via hieronder opgegeven extensies
     assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.png'],
     plugins: [vue()],
 });
