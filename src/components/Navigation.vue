0508f8fc79800701d7c287473757d7443ab01f97
NickBevers <nick.bevers9@gmail.com>
2022-12-15 09:01:24 +0100

    fix(navigation): import router for the redirecting

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <script setup>
     import { ref } from 'vue'
-    import router from '../../router'
+    import router from '../router'
     const isAdmin = ref(false);
     const jwtToken = ref(localStorage.getItem('jwtToken'));
     
